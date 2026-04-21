f454639b5253f447f8889fa6b7390f9a42ff8bc8
Sigbenu <oghenero.sigbenu@seerglobalsolutions.com>
2026-04-21 22:56:28 +0100

    update server.js-2

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -67,10 +67,6 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
   },
   pool: {
-    // max: 10,
-    // min: 0,
-    // acquire: 30000,
-    // idle: 10000,
     max: 5,
     min: 0,
     acquire: 10000,
